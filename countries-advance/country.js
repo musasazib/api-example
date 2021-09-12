b922236714c371636b751ab5678de9920bd69fc2
musasazib <musasazib@gmail.com>
2021-09-13 00:04:13 +0600

    Display countries name, capitald - API data

--- countries-advance/country.js
+++ countries-advance/country.js
@@ -8,22 +8,10 @@ loadCountries();
 
 const displayCountries = counties => {
     const countriesDiv = document.getElementById('countries');
-    // for (const country of counties) {
-    //     console.log(country);
-    // }\
     counties.forEach(country => {
         const div = document.createElement('div');
-
         div.classList.add('countries')
-
-        const h3 = document.createElement('h3');
-        h3.innerText = country.name;
-        div.appendChild(h3);
-
-        const p = document.createElement('p');
-        p.innerText = country.capital;
-        div.appendChild(p);
-
+        
         countriesDiv.appendChild(div);
     })
 }
