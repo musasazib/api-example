53c5d2783c9ac02320b3fc3e05a81333166f49e0
musasazib <musasazib@gmail.com>
2021-09-13 00:42:24 +0600

    Advanced - Call dynamic API, load dynamic data to display

--- countries-advance/country.js
+++ countries-advance/country.js
@@ -11,7 +11,27 @@ const displayCountries = counties => {
     counties.forEach(country => {
         const div = document.createElement('div');
         div.classList.add('countries')
-        
+        div.innerHTML = `
+        <h3>${country.name}</h3>
+        <p>${country.capital}</p>
+        <button onclick="loadCountryByName('${country.name}')">Details</button>
+        `
         countriesDiv.appendChild(div);
     })
+}
+
+const loadCountryByName = name => {
+    const url = `https://restcountries.eu/rest/v2/name/${name}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data =>displayCountryDetail(data[0]))
+}
+
+const displayCountryDetail = country => {
+    const countryDiv = document.getElementById('country-detail');
+    countryDiv.innerHTML = `
+    <h4>${country.name}</h4>
+    <p>Population: ${country.population }</p>
+    <img width="200px" src="${country.flag}">
+    `
 }
